afdef74bcb66d34972e5f4136f19bf6bd6b8e9da
zulfikar11233 <zulfikar11233@gmail.com>
2026-03-28 19:16:46 +0530

    error loading

--- next.config.js
+++ next.config.js
@@ -69,7 +69,6 @@ const nextConfig = {
   source: '/_next/static/:path*',
   headers: [
     { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
-    { key: 'Content-Type', value: 'application/javascript' },
   ],
 },
     {
